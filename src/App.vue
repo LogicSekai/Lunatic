df8987f686b66c8138b687b53222d29a709df0d4
LogicSekai <cameliayu112@gmail.com>
2023-04-20 17:25:00 +0700

    Navbar router-link props

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,16 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div class="flex">
     <div class="w-[240px] h-screen bg-vulcan-950 flex flex-col">
       <!-- <img class="w-auto" src="https://nisamerica.com/crymachina/images/crymachina-logo.png" alt=""> -->
-      <div class="navbar mt-5">
-        <div class="pl-6 py-2 text-vulcan-100 flex border-l-2 border-vulcan-800">
-          <svg class="w-[25px] h-[25px]" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
+      <div class="h-[100px]"></div>
+      <Navbar :navLinks="navLinks" />
+      <hr class="mt-4 text-vulcan-900 mx-4">
+      <div class="credit text-vulcan-100">
+        <div class="bg-vulcan-700 p-2 rounded-lg flex">
+          <svg class="w-[50px] h-50" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z"></path>
           </svg>
-          <div class="ml-4">Beranda</div>
-        </div>
-        <div class="pl-6 py-2 text-vulcan-100 flex">
-          <svg class="w-[25px] h-[25px]" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
-          </svg>
-          <div class="ml-4">Jadwal Tayang</div>
+          <div>GitHub</div>
         </div>
       </div>
       <div class="w-full mt-auto mb-5 flex justify-center">
@@ -29,75 +21,31 @@ import HelloWorld from './components/HelloWorld.vue'
         </div>
       </div>
     </div>
-    <div class="bg-vulcan-950 w-full">
-      <div class="w-full h-[56px] bg-vulcan-600">
-
-      </div>
+    <div class="bg-vulcan-950 w-full text-vulcan-100">
+      <div class="w-full h-[56px] bg-vulcan-600"></div>
+      <router-view />
     </div>
   </div>
   <!-- <RouterView /> -->
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+import Navbar from "./components/Navbar.vue";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+export default {
+  components: {
+    Navbar,
+  },
+  data() {
+    return {
+      navLinks: [
+        { text: "Home", path: "/" },
+        { text: "Reverse Search", path: "/reverse-search" },
+        { text: "Waifu", path: "/waifu" },
+        { text: "Schedule", path: "/schedule" },
+        { text: "Whastapp BOT", path: "/whatsapp" },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="navbar mt-5 font-semibold text-vulcan-100/60 cursor-pointer">
+        <router-link v-for="link in navLinks" :key="link.text" :to="link.path" class="pl-6 py-2 flex items-center hover:bg-vulcan-600" active-class="text-vulcan-100 relative before:w-[4px] before:h-full before:absolute before:left-0 before:bg-vulcan-400">
+            <svg class="w-[25px] h-[25px]" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path v-if="link.text === 'Home'" stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
+                <path v-else-if="link.text === 'Reverse Search'" stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
+                <path v-else-if="link.text === 'Waifu'" stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"></path>
+                <path v-else-if="link.text === 'Schedule'" stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"></path>
+                <path v-else-if="link.text === 'Whastapp BOT'" stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"></path>
+            </svg>
+            <div class="ml-4">{{ link.text }}</div>
+        </router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            navLinks: {
+                type: Array,
+                required: true,
+            },
+        },
+    };
+</script>
